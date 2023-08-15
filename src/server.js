346810b5a24128ce9389041a60e1e63e647a2bd0
lucastenani <lucas.tenani3@gmail.com>
2023-08-15 20:53:15 -0300

    User creation

--- src/server.js
+++ src/server.js
@@ -1,10 +1,17 @@
 import http from "node:http";
 
 const server = http.createServer((request, response) => {
+  const { method, url } = request;
+
+  if (method === "GET" && url === "/users") {
+    return response.end("List of users");
+  }
+
+  if (method === "POST" && url === "/users") {
+    return response.end("User creation");
+  }
+
   return response.end("Hello, World!");
 });
 
 server.listen(3333);
-
-// CommonJS => require
-// ESModules => import/export
