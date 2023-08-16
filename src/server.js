b44f363525cdd8978531b4ff4115b82e17361e74
lucastenani <lucas.tenani3@gmail.com>
2023-08-15 21:08:56 -0300

    creating and returning a user

--- src/server.js
+++ src/server.js
@@ -1,14 +1,18 @@
 import http from "node:http";
 
+const users = [];
+
 const server = http.createServer((request, response) => {
   const { method, url } = request;
 
   if (method === "GET" && url === "/users") {
-    return response.end("List of users");
+    return response.end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
-    return response.end("User creation");
+    users.push({ id: 1, name: "John Doe", email: "john.doe@example.com" });
+
+    return response.end("User created");
   }
 
   return response.end("Hello, World!");
