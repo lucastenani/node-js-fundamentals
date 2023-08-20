ccf80c06f6acc30f93dcb2270ddf70e4a5c7a70e
lucastenani <lucas.tenani3@gmail.com>
2023-08-19 21:55:00 -0300

    Writable Streams

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -2,7 +2,7 @@
 
 // process.stdin.pipe(process.stdout);
 
-import { Readable } from "node:stream";
+import { Readable, Writable } from "node:stream";
 
 class OneToHundredStream extends Readable {
   index = 1;
@@ -20,4 +20,11 @@ class OneToHundredStream extends Readable {
   }
 }
 
-new OneToHundredStream().pipe(process.stdout);
+class MultiplyByTenStream extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString()) * 10);
+    callback();
+  }
+}
+
+new OneToHundredStream().pipe(new MultiplyByTenStream());
