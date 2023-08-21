b1036ba16c923abc29539f1449a2f85da164cde3
lucastenani <lucas.tenani3@gmail.com>
2023-08-21 15:34:32 -0300

    transfrom streams

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -2,7 +2,7 @@
 
 // process.stdin.pipe(process.stdout);
 
-import { Readable, Writable } from "node:stream";
+import { Readable, Transform, Writable } from "node:stream";
 
 class OneToHundredStream extends Readable {
   index = 1;
@@ -20,6 +20,14 @@ class OneToHundredStream extends Readable {
   }
 }
 
+class InverseNumberStream extends Transform {
+  _transform(chunk, encoding, callback) {
+    const transformed = Number(chunk.toString()) * -1;
+
+    callback(null, Buffer.from(String(transformed)));
+  }
+}
+
 class MultiplyByTenStream extends Writable {
   _write(chunk, encoding, callback) {
     console.log(Number(chunk.toString()) * 10);
@@ -27,4 +35,6 @@ class MultiplyByTenStream extends Writable {
   }
 }
 
-new OneToHundredStream().pipe(new MultiplyByTenStream());
+new OneToHundredStream()
+  .pipe(new InverseNumberStream())
+  .pipe(new MultiplyByTenStream());
