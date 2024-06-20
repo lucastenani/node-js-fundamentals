7a17068d5ad6f8f31a7b3bd6aa35353b10d365b8
lucastenani <lucas.tenani3@gmail.com>
2024-06-20 11:52:47 -0300

    Hello, World

---
+++ src/server.js
@@ -0,0 +1,10 @@
+// commonJS => require
+// ESModules => import/export
+
+import http from 'node:http'
+
+const server = http.createServer((req, res) => {
+  return res.end('Hello, World!')
+})
+
+server.listen(3333) // localhost:3333
