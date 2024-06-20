338be2fada4bbc68c9c29bb873668ec9d6cc4143
lucastenani <lucas.tenani3@gmail.com>
2024-06-20 12:17:24 -0300

    Creation and listing routes (HTTP Methods)

--- src/server.js
+++ src/server.js
@@ -1,9 +1,25 @@
 // commonJS => require
 // ESModules => import/export
 
+// GET - Fetch a resource
+// POST - Create something
+// PUT - Edit or update a resource
+// PATCH - Update unique information about a resource
+// DELETE - Delete a resource
+
 import http from 'node:http'
 
 const server = http.createServer((req, res) => {
+  const { method, url } = req
+
+  if (method === 'GET' && url === '/users') {
+    return res.end('Users List')
+  }
+
+  if (method === 'POST' && url === '/users') {
+    return res.end('Create user')
+  }
+
   return res.end('Hello, World!')
 })
 
